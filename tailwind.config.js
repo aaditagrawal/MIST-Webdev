d71759e4a7bab4cfa4ac555e1809f479cb6c4c79
hello <mav@aurion.taild8fcc.ts.net>
2024-11-02 17:37:55 +0530

    comments here

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: ["./*.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
 
